6905f813bdcb56b4004714d35af6faab29a673df
JensDeNijs <jensdenijs@hotmail.com>
2021-06-30 16:31:41 +0200

    added extra files

---
+++ class/DiscountOld.js
@@ -0,0 +1,21 @@
+/*
+import {DiscountType} from "./Discount";
+
+export class DiscountOld {
+    private _type: DiscountType;
+    private _value: number;
+
+    constructor(type : DiscountType, value : number = 0) {
+        this._type = type;
+        this._value = value;
+    }
+
+    get type(): DiscountType {
+        return this._type;
+    }
+
+    get value(): number {
+        return this._value;
+    }
+
+}*/
